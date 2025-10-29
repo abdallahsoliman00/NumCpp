b19d33711e1270754ee938c4de2c6df81fb1bc4b
abdallahsoliman00 <abdallahsoliman03@gmail.com>
2025-10-29 15:44:10 +0300

    Added the inverse DFT

--- NumCpp/FFT.hpp
+++ NumCpp/FFT.hpp
@@ -31,6 +31,24 @@ namespace numcpp::fft {
         return dft_out;
     }
 
+    template <typename T, typename = std::enable_if_t<std::is_arithmetic_v<T>>>
+    NArray<complex<T>> idft(const NArray<complex<T>>& X_k) {
+        const size_t N_samples = X_k.get_total_size();
+        NArray<complex<T>> x_n(Shape{N_samples});
+
+        for (size_t n = 0; n < N_samples; n++) {
+            complex<T> sum = 0;
+            for (size_t k = 0; k < N_samples; k++) {
+                T angle = (M_2PI * static_cast<T>(k) * static_cast<T>(n) / N_samples);
+                sum += X_k.get_data()[k] * complex<T>(std::cos(angle), std::sin(angle));
+            }
+            x_n(n) = sum / static_cast<T>(N_samples);
+        }
+        return x_n;
+    }
+
+
+
     inline NArray<double> fftfreq(const size_t& n_samples, const float& diff = 1.0f) {
         const size_t N_2 = (n_samples + 1) / 2;
         NArray<double> out(Shape{n_samples});
